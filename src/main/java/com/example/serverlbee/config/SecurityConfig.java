24160a0ae7e5e58c50829598f96c39a9afc13cea
thanvanlong <longthan366@gmail.com>
2022-07-18 13:44:08 +0700

    deploy 18/07

--- src/main/java/com/example/serverlbee/config/SecurityConfig.java
+++ src/main/java/com/example/serverlbee/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost"));
+        configuration.setAllowedOrigins(List.of("https://foodauth-1551d.web.app/", "http://localhost"));
         configuration.setAllowedMethods(List.of("HEAD",
                 "GET", "POST", "PUT", "DELETE", "PATCH"));
 //        // setAllowCredentials(true) is important, otherwise:
